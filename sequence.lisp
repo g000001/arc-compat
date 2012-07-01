452dc07db5392995a20e076fa4dd16408a7563f1
g000001 <chiba.masaomi@gmail.com>
2012-07-01 20:16:39 +0900

    fix: (+): compiler macro's bug

--- sequence.lisp
+++ sequence.lisp
@@ -8,8 +8,27 @@
     (cl:list (cl:append x y))
     (cl:string (cl:concatenate 'cl:string x y))))
 
+
+(define-compiler-macro x+y (&whole form x y)
+  (cond ((or (numberp x) (numberp y))
+         `(cl:+ ,x ,y))
+        ((or (stringp x) (stringp y))
+         `(cl:concatenate 'cl:string ,x ,y))
+        ((or (typep x '(cons (eql the) (cons (cl:member cl:number cl:fixnum))))
+             (typep y '(cons (eql the) (cons (cl:member cl:number cl:fixnum)))))
+         `(cl:+ ,x ,y))
+        (T form)))
+
+
 (def + obj
   (cl:reduce #'x+y obj))
+
+
+(define-compiler-macro + (&whole form &rest args)
+  (destructuring-bind (x y) args
+    (if (cl:= 2 (length args))
+        `(x+y ,x ,y)
+        form )))
 
 
 (def recstring (test s (o start 0))
