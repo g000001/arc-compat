fcdb2526783ee300838a600427728c8e49c184e5
g000001 <chiba.masaomi@gmail.com>
2012-08-28 17:03:09 +0900

    add: syntax: !, ., ∘

--- readtable.lisp
+++ readtable.lisp
@@ -46,28 +46,135 @@
   (:case :upcase))
 
 
+(defun arc-compat.internal::chars->value (chars) 
+  (read-from-string (coerce chars 'string)))
+
+
+(defun arc-compat.internal::symbol->chars (x) 
+  (coerce (string x) 'list))
+
+
+(defun arc-compat.internal::insym? (char sym)
+  (member char (arc-compat.internal::symbol->chars sym)))
+
+
+(defun arc-compat.internal::tokens (test source token acc keepsep?)
+  (cond ((null source)
+         (reverse (if (consp token) 
+                      (cons (reverse token) acc)
+                      acc)))
+        ((funcall test (car source))
+         (arc-compat.internal::tokens test
+                 (cdr source)
+                 '()
+                 (let ((rec (if (null token)
+                            acc
+                            (cons (reverse token) acc))))
+                   (if keepsep?
+                       (cons (car source) rec)
+                       rec))
+                 keepsep?))
+        (T (arc-compat.internal::tokens test
+                   (cdr source)
+                   (cons (car source) token)
+                   acc
+                   keepsep?))))
+
+
+(defun arc-compat.internal::has-ssyntax-char? (string i)
+  (and (>= i 0)
+       (or (let ((c (char string i)))
+             (or (eql c #\∘) (eql c #\~) ;(eqv? c #\_) 
+                 (eql c #\.)  (eql c #\!)))
+           (arc-compat.internal::has-ssyntax-char? string (- i 1)))))
+
+
+(defun arc-compat.internal::ssyntax? (x)
+  (and (symbolp x)
+       (not (or (eql x 'arc:+) (eql x 'arc:++) (eql x 'arc::_)))
+       (let ((name (string x)))
+         (arc-compat.internal::has-ssyntax-char? name (- (length name) 1)))))
+
+
+(defun arc-compat.internal::expand-compose (sym)
+  (let ((elts (mapcar (lambda (tok)
+                        (if (eql (car tok) #\~)
+                         (if (null (cdr tok))
+                             'arc:no
+                             `(cl:complement 
+                               #',(arc-compat.internal::chars->value (cdr tok))))
+                         (arc-compat.internal::chars->value tok)))
+                      (arc-compat.internal::tokens 
+                       (lambda (c) (eql c #\∘))
+                       (arc-compat.internal::symbol->chars sym) 
+                       '() 
+                       '() 
+                       'nil))))
+    (if (null (cdr elts))
+        (car elts)
+        (let ((args (gensym)))
+          `(lambda (&rest ,args)
+             (declare (dynamic-extent ,args))
+             (apply (arc::compose ,@(mapcar (lambda (e) `#',e) 
+                                            elts))
+                    ,args) )))))
+
+
+(defun arc-compat.internal::expand-ssyntax (sym)
+  (funcall 
+   (cond ((or (arc-compat.internal::insym? #\∘ sym) 
+              (arc-compat.internal::insym? #\~ sym))
+          #'arc-compat.internal::expand-compose)
+     ;   ((insym? #\_ sym) expand-curry)
+         ((or (arc-compat.internal::insym? #\. sym)
+              (arc-compat.internal::insym? #\! sym))
+          #'arc-compat.internal::expand-sexpr)
+         (T (error "~A Unknown ssyntax" sym)))
+   sym))
+
+
+(defun arc-compat.internal::build-sexpr (toks orig)
+  (cond ((null toks)
+         'arc::get)
+        ((null (cdr toks))
+         (arc-compat.internal::chars->value (car toks)))
+        (T (if (eql (cadr toks) #\!)
+               (list 'arc-compat.internal::ref
+                     (arc-compat.internal::build-sexpr (cddr toks) orig)
+                     (list 'cl:quote
+                           (arc-compat.internal::chars->value (car toks))))
+               (if (or (eql (car toks) #\.) (eql (car toks) #\!))
+                   (error "~A Bad ssyntax" orig)
+                   (list 
+                    (arc-compat.internal::build-sexpr (cddr toks) orig)
+                    (arc-compat.internal::chars->value (car toks))))))))
+
+
+(defun arc-compat.internal::expand-sexpr (sym)
+  (arc-compat.internal::build-sexpr
+   (reverse (arc-compat.internal::tokens
+             (lambda (c) (or (eql c #\.) (eql c #\!)))
+             (arc-compat.internal::symbol->chars sym)
+             '()
+             '()
+             'T))
+   sym))
+
+
 (let ((r (copy-readtable nil)))
   (defun arc-compat.internal::read-symbol (stream)
     (let* ((*readtable* r)
            (obj (read-preserving-whitespace stream)) )
       (typecase obj
-        (symbol (let ((symname (ppcre:split #\∘ (symbol-name obj))))
-                  (if (cdr symname)
-                      (values (mapcar (lambda (s) `#',(intern s)) symname) t)
-                      obj )))
+        (symbol (if (arc-compat.internal::ssyntax? obj)
+                    (arc-compat.internal::expand-ssyntax obj)
+                    obj))
         (otherwise obj) ))))
 
 
 (defun arc-compat.internal::compose-reader-macro-reader (stream char)
   (unread-char char stream)
-  (multiple-value-bind (expr win)
-                       (arc-compat.internal::read-symbol stream)
-    (if win
-        (let ((args (gensym)))
-          `(lambda (&rest ,args)
-             (declare (dynamic-extent ,args))
-             (apply (arc::compose ,@expr) ,args) ))
-        expr )))
+  (arc-compat.internal::read-symbol stream))
 
 
 ;;; eof
