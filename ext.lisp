40b8ccc35df62f1314a011e9ade1071e5cb6061c
g000001 <chiba.masaomi@gmail.com>
2012-12-21 19:05:33 +0900

    add: *let (multiple-value-bind)

--- ext.lisp
+++ ext.lisp
@@ -1,9 +1,12 @@
 (in-package :arc-compat.internal)
 (in-readtable :common-lisp)
 
+
 (defalias =* cl:defvar)
 
 
 (defalias *let cl:multiple-value-bind)
 
 
+(def ignore-vars (vars)
+  `(cl:declare (cl:ignore ,@vars)))
