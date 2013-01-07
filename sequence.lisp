c71cfe9717e8d018bbc38da7c261d697cfc50156
g000001 <chiba.masaomi@gmail.com>
2013-01-07 23:45:42 +0900

    clean up

--- sequence.lisp
+++ sequence.lisp
@@ -3,35 +3,5 @@
 (in-suite arc-compat)
 
 
-(def x+y (x y)
-  (etypecase x
-    (cl:number (cl:+ x y))
-    (cl:list (cl:append x y))
-    (cl:string (cl:concatenate 'cl:string x y))))
-
-
-(define-compiler-macro x+y (&whole form x y)
-  (cond ((or (numberp x) (numberp y))
-         `(cl:+ ,x ,y))
-        ((or (stringp x) (stringp y))
-         `(cl:concatenate 'cl:string ,x ,y))
-        ((or (typep x 
-                    '(cons (eql cl:the) (cons (cl:member cl:number cl:fixnum))))
-             (typep y '(cons (eql cl:the) (cons (cl:member cl:number cl:fixnum)))))
-         `(cl:+ ,x ,y))
-        (T form)))
-
-
-(def + obj
-  (cl:reduce #'x+y obj))
-
-
-(define-compiler-macro + (&whole form &rest args)
-  (cl:if (cl:= 2 (length args))
-         (destructuring-bind (x y) args
-           `(x+y ,x ,y))
-         form ))
-
-
 ;;; eof
 
